6917840eb5fe0734f53e16ac0a5351310a2f67a2
Arkaprabha Ghosh <arkag70@gmail.com>
2019-07-08 20:05:43 +0530

    updated with image and names

--- test.py
+++ test.py
@@ -1,13 +1,35 @@
 import win32com.client as win32
 import pandas as pd
 from datetime import datetime
+import os
+import random
 
+def html_src(names):
+	
+	files = []
+	files = os.listdir("Cards\\")
+	path = "\\\\bosch.com\\dfsrb\\DfsIN\\LOC\\Kor\\NE2\\ESV_Info\\36_ESX\\Common\\General_Information\\DASy\\09_TeamDetails\\ESX3BirthdayList\\Cards\\"+str(random.choice(files))
+
+	html_content = f'''
+
+	<!DOCTYPE html>
+	<html>
+		<head> 
+		</head>
+		<body>
+			<h2 style ="font-family:'Comic Sans MS', cursive, sans-serif; color:#005cde; font-size:48px">Many many happy returns of the Day {names}</h2>
+				<img src = {path} ></img>
+
+		</body>
+	</html>'''
+	return html_content
 
 def getDataFrame():
 
 	to = ""
 	cc = ""
-	data = pd.read_excel("team.xlsx", sheet_name="Sheet1")
+	names = ""
+	data = pd.read_excel("\\\\bosch.com\\dfsrb\\DfsIN\\LOC\\Kor\\NE2\\ESV_Info\\36_ESX\\Common\\General_Information\\DASy\\09_TeamDetails\\ESX3BirthdayList\\team.xlsx", sheet_name="Sheet1")
 	todaysdate = str(datetime.now()).split(' ')[0]
 	time = str(datetime.now()).split(' ')[1]
 	thismonth = int(todaysdate.split('-')[1])
@@ -18,28 +40,34 @@ def getDataFrame():
 		birthdate = str(data.iloc[entry]["DOB"]).split(" ")[0]
 		birthmonth = int(birthdate.split("-")[1])
 		birthday = int(birthdate.split("-")[2])
+		# name = str(data.iloc[entry]["Name"])
+		# print(f"{name}  date : {birthday} month : {birthmonth}")
 
 		if(thismonth == birthmonth and thisday == birthday):
-			to += data.iloc[entry]["Name"]+"; "
+			to += str(data.iloc[entry]["Mail_ID"])+"; "
+			names += str(data.iloc[entry]["Name"])+"; "
 		else:
-			cc += data.iloc[entry]["Mail_ID"]+"; "
-	names = " ".join([x for x in to.split("; ")])
-	sub = f"Happy Birthday to : {names}"
-	print(f"To : {to}")
-	print(f"CC : {cc}")
-	print(f"Sub: {sub}")
+			if "bosch.com" in str(data.iloc[entry]["Mail_ID"]):
+				cc += str(data.iloc[entry]["Mail_ID"])+"; "
+
+	names = " and ".join([x for x in names.split("; ")]) 
+	names = names[:-4]
+	sub = f"Happy Birthday {names}"
+	body = html_src(names)
+	mail_func(to,"",sub,body)
+	
 	 
 	
 	
 
-def mail(to,cc,sub,body):
+def mail_func(to,cc,sub,body):
 	outlook = win32.Dispatch('outlook.application')
 	mail = outlook.CreateItem(0)
 	mail.To = to # separated by semi colon
-	mail.CC = cc
+	mail.CC = ""
 	mail.Subject = sub
 	mail.Body = body
-	mail.HTMLBody = f'<h2>{mail.body}</h2>' #this field is optional
+	mail.HTMLBody = body #this field is optional
 
 	# To attach a file to the email (optional):
 
@@ -47,3 +75,4 @@ def mail(to,cc,sub,body):
 
 if __name__ == "__main__":
 	getDataFrame()
+	#Arkaprabha.ghosh@in.bosch.com
